c8a8b178ea724eea9f1cb1fd6a3ec4a769cbb4ed
Priyanshu Kumar <priyanshumails.bca2025@gmail.com>
2024-11-14 18:28:16 +0530

    Add files via upload

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      Welcome to our Event Management Platform, where every detail of your event is taken care of with ease and precision. Whether you're planning an intimate celebration or a grand corporate event, we provide a complete set of tools to make your journey stress-free. From venue selection and vendor coordination to guest management and budget tracking, our platform helps you stay organized every step of the way. With our intuitive interface and responsive customer support, we ensure that your event comes together effortlessly. Let us help you create remarkable memories and unforgettable experiences, tailored to your needs!
+      </p>
+    </section>
+  );
+};
+
+export default About;
